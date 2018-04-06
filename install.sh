621319ce6130c270bffc90674a1192714923968b
Tom Herbers <git@tomherbers.de>
2018-04-06 21:20:58 +0200

    install some stuff only if not already installed ither fixes

--- install.sh
+++ install.sh
@@ -25,22 +25,28 @@ apt upgrade -y
 apt install htop zip unzip locate git -y
 updatedb
 
-#install docker
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-apt update
-apt install docker-ce -y
-docker run hello-world
+#install docker if neccesary
+if hash docker>/dev/null;
+  then
+    echo "Docker will now be installed"
+    apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/debian \
+       $(lsb_release -cs) \
+       stable"
+    apt update
+    apt install docker-ce -y
+    docker run hello-world
+    if hash docker>/dev/null; then echo "Docker wasn't installed with success!"; exit 1; fi
+  else
+    echo "Docker was already installed on the system"
+fi
 
 InstallWebserver=false
-
 beep
-read -r -p "Do you want to install Apache2 and PHP7.0? [y/N] " response
+read -r -p "Do you want to install Apache2 and PHP7.0 as well as certbot? [y/N] " response
   case $response in
     [yY][eE][sS]|[yY])
       echo "Apache2 and PHP7.0 will now be installed"
@@ -66,97 +72,99 @@ read -r -p "Do you want to install Apache2 and PHP7.0? [y/N] " response
   esac
 
 InstallMailcow=false
+if [ ! -d "/opt/mailcow-dockerized/" ]; then
+  beep
+  read -r -p "Do you want to install Mailcow (a Mail Server with Web Frontend)? [y/N] " response
+    case $response in
+      [yY][eE][sS]|[yY])
+        InstallMailcow=true
+        echo "Mailcow will now be installed"
 
-beep
-read -r -p "Do you want to install Mailcow (a Mail Server with Web Frontend)? [y/N] " response
-  case $response in
-    [yY][eE][sS]|[yY])
-      InstallMailcow=true
-      echo "Mailcow will now be installed"
+        #install docker-compose
+        echo "Docker Compose is now installed"
+        curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
 
-      #install docker-compose
-      echo "Docker Compose is now installed"
-      curl -L https://github.com/docker/compose/releases/download/$(curl -Ls https://www.servercow.de/docker-compose/latest.php)/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose
+        #clone mailcow
+        cd /opt
+        git clone https://github.com/mailcow/mailcow-dockerized
+        cd mailcow-dockerized
+        beep
+        ./generate_config.sh
 
-      #clone mailcow
-      cd /opt
-      git clone https://github.com/mailcow/mailcow-dockerized
-      cd mailcow-dockerized
-      beep
-      ./generate_config.sh
+        source /opt/mailcow-dockerized/mailcow.conf
 
-      source /opt/mailcow-dockerized/mailcow.conf
+        #if WebServer is installed the MailServer Web Frontend will be Used by a Proxy
+        if [[ $InstallWebserver=true ]]; then
+          sed -i "s/\(HTTP_PORT *= *\).*/\18080/" /opt/mailcow-dockerized/mailcow.conf
+          sed -i "s/\(HTTP_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
+          sed -i "s/\(HTTPS_PORT *= *\).*/\18443/" /opt/mailcow-dockerized/mailcow.conf
+          sed -i "s/\(HTTPS_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
 
-      #if WebServer is installed the MailServer Web Frontend will be Used by a Proxy
-      if [[ $InstallWebserver=true ]]; then
-        sed -i "s/\(HTTP_PORT *= *\).*/\18080/" /opt/mailcow-dockerized/mailcow.conf
-        sed -i "s/\(HTTP_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
-        sed -i "s/\(HTTPS_PORT *= *\).*/\18443/" /opt/mailcow-dockerized/mailcow.conf
-        sed -i "s/\(HTTPS_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
+          sed -i "s/\(SKIP_LETS_ENCRYPT *= *\).*/\1y/" /opt/mailcow-dockerized/mailcow.conf
 
-        sed -i "s/\(SKIP_LETS_ENCRYPT *= *\).*/\1y/" /opt/mailcow-dockerized/mailcow.conf
+          sitesAvailabledomain='/etc/apache2/sites-available/'$MAILCOW_HOSTNAME.conf
+          echo "Creating a vhost for $MAILCOW_HOSTNAME"
 
-        sitesAvailabledomain='/etc/apache2/sites-available/'$MAILCOW_HOSTNAME.conf
-        echo "Creating a vhost for $MAILCOW_HOSTNAME"
+          ### create virtual host rules file
 
-        ### create virtual host rules file
+          maindomain=$(expr match "$MAILCOW_HOSTNAME" '.*\.\(.*\..*\)')
+          echo "
+              <VirtualHost *:80>
+                ServerName $MAILCOW_HOSTNAME
+                ServerAlias autodiscover.$maindomain
+                ServerAlias autoconfig.$maindomain
+                DocumentRoot /var/www/html
+              </VirtualHost>" > $sitesAvailabledomain
+          echo "New Virtual Host Created"
 
-        maindomain=$(expr match "$MAILCOW_HOSTNAME" '.*\.\(.*\..*\)')
-        echo "
-            <VirtualHost *:80>
-              ServerName $MAILCOW_HOSTNAME
-              ServerAlias autodiscover.$maindomain
-              ServerAlias autoconfig.$maindomain
-              DocumentRoot /var/www/html
-            </VirtualHost>" > $sitesAvailabledomain
-        echo "New Virtual Host Created"
+          a2ensite $MAILCOW_HOSTNAME
+          service apache2 reload
 
-        a2ensite $MAILCOW_HOSTNAME
-        service apache2 reload
+          #certbot --apache
+        fi
 
-        #certbot --apache
+        #nano mailcow.conf
+        docker-compose pull
+        #the following command must be run to start Mailcow 'docker-compose up -d'
 
-      fi
-
-      #nano mailcow.conf
-      docker-compose pull
-      #the following command must be run to start Mailcow 'docker-compose up -d'
-
+        ;;
+      *)
+        echo "Malicow won't be installed"
       ;;
-    *)
-      echo "Malicow won't be installed"
-    ;;
-  esac
+    esac
+fi
 
-beep
-read -r -p "Do you want to install webmin? [y/N] " response
-  case $response in
-    [yY][eE][sS]|[yY])
-      echo "Webmin will now be installed"
-      #install webmin
-      echo 'deb https://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
-      curl -fsSL http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
-      apt update
-      apt install webmin -y
+if [ ! -d "/etc/webmin/" ]; then
+  beep
+  read -r -p "Do you want to install webmin? [y/N] " response
+    case $response in
+      [yY][eE][sS]|[yY])
+        echo "Webmin will now be installed"
+        #install webmin
+        echo 'deb https://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
+        curl -fsSL http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
+        apt update
+        apt install webmin -y
 
-      #only allow localhost for Webmin Access
-      echo 'allow=127.0.0.1' >> /etc/webmin/miniserv.conf
-      echo 'trust_real_ip=0' >> /etc/webmin/miniserv.conf
+        #only allow localhost for Webmin Access
+        echo 'allow=127.0.0.1' >> /etc/webmin/miniserv.conf
+        echo 'trust_real_ip=0' >> /etc/webmin/miniserv.conf
 
-      #disable SSL in Webmin
-      sed -i "s/\(ssl *= *\).*/\10/" /etc/webmin/miniserv.conf
-      /etc/webmin/restart
+        #disable SSL in Webmin
+        sed -i "s/\(ssl *= *\).*/\10/" /etc/webmin/miniserv.conf
+        /etc/webmin/restart
+        ;;
+      *)
+        echo "Webmin won't be installed"
       ;;
-    *)
-      echo "Webmin won't be installed"
-    ;;
-  esac
+    esac
+fi
 
 echo ""
 echo "Scipt finished! Thanks for using! I hope everything works!"
 
-if [[ $InstallMailcow=true ]]; then
+if [ $inst == "true" ]; then
   echo "to start Mailcow the following command must be run 'docker-compose up -d'."
   echo "This will allow you to acces it. The default credentials are admin/moohoo'."
 fi
