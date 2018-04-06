996558d20770001681b6d2c90c5827ee8818247d
Tom Herbers <git@tomherbers.de>
2018-04-06 13:01:45 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -28,9 +28,17 @@ chmod +x /usr/local/bin/docker-compose
 cd /opt
 git clone https://github.com/mailcow/mailcow-dockerized
 cd mailcow-dockerized
-#./generate_config.sh
+./generate_config.sh
+sed -i "s/\(HTTP_PORT *= *\).*/\18080/" /opt/mailcow-dockerized/mailcow.conf
+sed -i "s/\(HTTP_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
+sed -i "s/\(HTTPS_PORT *= *\).*/\18443/" /opt/mailcow-dockerized/mailcow.conf
+sed -i "s/\(HTTPS_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
+
+sed -i "s/\(SKIP_LETS_ENCRYPT *= *\).*/\1y/" /opt/mailcow-dockerized/mailcow.conf
+
 #nano mailcow.conf
 docker-compose pull
+#the following command must be run to start Mailcow 'docker-compose up -d'
 
 #install webmin
 echo 'deb https://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
@@ -44,3 +52,17 @@ echo 'trust_real_ip=0' >> /etc/webmin/miniserv.conf
 #disable SSL in Webmin
 sed -i "s/\(ssl *= *\).*/\10/" /etc/webmin/miniserv.conf
 /etc/webmin/restart
+
+#install Apache2 and PHP
+apt install apache2 php7.0 -y
+systemctl restart apache2
+a2enmod rewrite
+a2enmod headers
+a2enmod http2
+a2enmod ssl
+
+#install certbot
+apt install python-certbot-apache -t stretch-backports -y
+
+#restart Apache2
+systemctl restart apache2
