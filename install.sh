bdc2e16dbeac65f17b22a75a1ca077ab52c1eaec
Tom Herbers <git@tomherbers.de>
2018-04-06 22:27:33 +0200

    put Docker install routine into function install docker during Mailcow install it if not already done before

--- install.sh
+++ install.sh
@@ -4,6 +4,20 @@ beep(){
   echo -en "\007"
 }
 
+installDocker(){
+  echo "Docker will now be installed"
+  apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+  apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
+  apt update
+  apt install docker-ce -y
+  docker run hello-world
+}
+
 beep
 read -r -p "Do you really want to start installing Software on the Server? [y/N] " response
   case $response in
@@ -25,32 +39,31 @@ apt upgrade -y
 apt install htop zip unzip locate git -y
 updatedb
 
+DockerInstalled=false
 #install docker if neccesary
 docker -v &> /dev/null
-if [ ! $? -eq 0 ];
-  then
-    echo "Docker will now be installed"
-    apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-    apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/debian \
-       $(lsb_release -cs) \
-       stable"
-    apt update
-    apt install docker-ce -y
-    docker run hello-world
+if [ ! $? -eq 0 ]; then
+  read -r -p "Do you want to install Docker? [y/N] " response
+    case $response in
+      [yY][eE][sS]|[yY])
+        installDocker
+        DockerInstalled=true
+        ;;
+      *)
+      ;;
+    esac
   else
-    echo "Docker is already installed on the system"
+    DockerInstalled=true
+    echo "Docker is already installed on the system. No need to install"
 fi
 
-InstallWebserver=false
+=false
 beep
 read -r -p "Do you want to install Apache2 and PHP7.0 as well as certbot? [y/N] " response
   case $response in
     [yY][eE][sS]|[yY])
       echo "Apache2 and PHP7.0 will now be installed"
-      InstallWebserver=false
+      WebserverInstalled=false
 
       #install Apache2 and PHP
       apt install apache2 php7.0 -y
@@ -70,14 +83,18 @@ read -r -p "Do you want to install Apache2 and PHP7.0 as well as certbot? [y/N] 
     ;;
   esac
 
-InstallMailcow=false
+MailcowInstalled=false
 if [ ! -d "/opt/mailcow-dockerized/" ]; then
   beep
   read -r -p "Do you want to install Mailcow (a Mail Server with Web Frontend)? [y/N] " response
     case $response in
       [yY][eE][sS]|[yY])
-        InstallMailcow=true
         echo "Mailcow will now be installed"
+
+        # install Docker if not installed
+        if [ $DockerInstalled == "true" ]; then
+          installDocker
+        fi
 
         #install docker-compose if not already installed
         if [ ! -d "/usr/local/bin/docker-compose" ]; then
@@ -96,7 +113,7 @@ if [ ! -d "/opt/mailcow-dockerized/" ]; then
         source /opt/mailcow-dockerized/mailcow.conf
 
         #if WebServer is installed the MailServer Web Frontend will be Used by a Proxy
-        if [[ $InstallWebserver=true ]]; then
+        if [[ $WebserverInstalled=true ]]; then
           sed -i "s/\(HTTP_PORT *= *\).*/\18080/" /opt/mailcow-dockerized/mailcow.conf
           sed -i "s/\(HTTP_BIND *= *\).*/\1127.0.0.1/" /opt/mailcow-dockerized/mailcow.conf
           sed -i "s/\(HTTPS_PORT *= *\).*/\18443/" /opt/mailcow-dockerized/mailcow.conf
@@ -128,7 +145,7 @@ if [ ! -d "/opt/mailcow-dockerized/" ]; then
         #nano mailcow.conf
         docker-compose pull
         #the following command must be run to start Mailcow 'docker-compose up -d'
-
+        MailcowInstalled=true
         ;;
       *)
       ;;
@@ -163,7 +180,7 @@ fi
 echo ""
 echo "Scipt finished! Thanks for using! I hope everything works!"
 
-if [ $InstallMailcow == "true" ]; then
+if [ $MailcowInstalled == "true" ]; then
   echo "to start Mailcow the following command must be run 'docker-compose up -d'."
   echo "This will allow you to acces it. The default credentials are admin/moohoo'."
 fi
